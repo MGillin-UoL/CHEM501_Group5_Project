f423dc056ea0054c3909378dc588255a984f347c
Michael Gillin <sgmgilli@liverpool.ac.uk>
2024-12-10 22:21:12 +0000

    Update CHEM501_MKR_Project_Code.ino

--- CHEM501_MKR_Project_Code.ino
+++ CHEM501_MKR_Project_Code.ino
@@ -11,8 +11,8 @@ MqttClient mqttClient(wifiClient);
 
 const char broker[] = "pf-eveoxy0ua6xhtbdyohag.cedalo.cloud";
 int        port     = 1883;
-const char topic1[] = "MGTemp"; // Temp
-const char topic2[] = "MGPressure"; // Pressure
+const char topic1[] = "Temp"; // Temp
+const char topic3[] = "Gas"; // Gas
 
 //set interval for sending messages (milliseconds)
 const long interval = 5000;
@@ -21,15 +21,11 @@ unsigned long previousMillis = 0;
 int count = 0;
 
 Sensor temp(SENSOR_ID_TEMP);
-Sensor pressure(SENSOR_ID_BARO);
 Sensor gas(SENSOR_ID_GAS);
 
 void setup() { 
    //Initialize serial and wait for port to open:
   Serial.begin(115200);
-//  while (!Serial) {
-//    ; // wait for serial port to connect. Needed for native USB port only
-//  }
 
   // attempt to connect to Wifi network:
   Serial.print("Attempting to connect to WPA SSID: ");
@@ -58,7 +54,7 @@ void setup() {
   Serial.begin(115200);
   BHY2Host.begin();
   temp.begin();
-  pressure.begin();
+//  pressure.begin();
   gas.begin();
 }
 
@@ -71,24 +67,8 @@ void loop() {
 
   if (currentMillis - previousMillis >= interval) {
     // save the last time a message was sent
-    previousMillis = currentMillis;
-  // // If prompted, collect some data
-  // if (Serial.available()) {
-    
-  //   // Reads input in form of (number of readings, readings per second)
-  //   // Find the number of readings to take
-  //   String numberOfReadingsString = Serial.readStringUntil(',');
-  //   int numberOfReadings = numberOfReadingsString.toInt();
-
-  //   // Find the delay time in ms between readings
-  //   String rateOfReadingsString = Serial.readStringUntil('\n');
-  //   float rateOfReadings = rateOfReadingsString.toFloat();
-  //   float delayTime = 1000.0/rateOfReadings;
-
-    // Make a note of the time at which data taking start
-    //unsigned long startTime = millis();
-    
-    send_data(5, 1000, millis()); //sends 5 readings, 1 second apart
+    previousMillis = currentMillis;   
+    send_data(1, 1000, millis()); //sends 1 reading, 1 second apart
   }
 } // end loop()
 
@@ -98,37 +78,21 @@ void send_data(int n_readings, float d_time, float t0){
 
   for (int n = 0; n < n_readings; n++)  {
     // Update the sensor readings 
-    BHY2Host.update();
-
-    // // Wait until it is time to take a new reading
-    // //change me
-    // while ( dataT < (n*d_time) ) {
-    //   delay(1);
-    //   dataT = millis() - t0;
-    
+    BHY2Host.update();    
 
     // Collect the data
     dataT = millis() - t0;
     float dataTemp = temp.value();
-    float dataP = pressure.value();
     float dataG = gas.value();
-
-    // Serial.print("Sending message to topic1: ");
-    // Serial.println(topic1);
-    // Serial.println(dataTemp);
 
     //sends temperature data to MQTT
     mqttClient.beginMessage(topic1);
     mqttClient.print(dataTemp);
     mqttClient.endMessage();
 
-    // Serial.print("Sending message to topic2: ");
-    // Serial.println(topic2);
-    // Serial.println(dataP);
-
-    //sends pressure data to MQTT
-    mqttClient.beginMessage(topic2);
-    mqttClient.print(dataP);
+    //sends gas data to MQTT
+    mqttClient.beginMessage(topic3);
+    mqttClient.print(dataG);
     mqttClient.endMessage();
 
     // Write the data to the serial port
@@ -136,11 +100,9 @@ void send_data(int n_readings, float d_time, float t0){
     Serial.print(millis() - t0);
     Serial.print("(ms), Temperature:");
     Serial.print(dataTemp);
-    Serial.print("(°C), Pressure:");
-    Serial.print(dataP);
-    Serial.print("(Pa), Gas:");
+    Serial.print("(°C), Gas:");
     Serial.print(dataG);
-    Serial.print("(ppb, maybe)");
+    Serial.print("(Ω)");
     Serial.print('\n');
   } // ends for() loop
 }
